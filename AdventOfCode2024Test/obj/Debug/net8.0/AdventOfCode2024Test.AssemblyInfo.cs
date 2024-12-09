551890be09cd61e5b26817e6dd074eb573f82a19
Edward Timgren <edward.timgren@inera.se>
2024-12-09 12:54:54 +0100

    git uppdate

--- AdventOfCode2024Test/obj/Debug/net8.0/AdventOfCode2024Test.AssemblyInfo.cs
+++ AdventOfCode2024Test/obj/Debug/net8.0/AdventOfCode2024Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2024Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e2690b9c31a7b3e447c70dd3d112a5c1fe9d43")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2024Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2024Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
